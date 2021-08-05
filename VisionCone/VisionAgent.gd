2caf755b20e505767419341cae395b0f1b99006c
Mick McCarthy <mick@mccarthy.tech>
2021-08-05 22:23:14 +0100

    2D vision detection

--- VisionCone/VisionAgent.gd
+++ VisionCone/VisionAgent.gd
@@ -1,16 +1,22 @@
 extends Spatial
 
+export var vision_cone_arc: float = 60.0
+var target: Spatial
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _ready() -> void:
+	target = get_tree().get_nodes_in_group('targets')[0]
 
+func _process(delta: float) -> void:
+	var target_point = target.global_transform.origin
+	if in_vision_cone(target_point):
+		$Graphics/Red.show()
+		$Graphics/Yellow.hide()
+	else:
+		$Graphics/Red.hide()
+		$Graphics/Yellow.show()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func in_vision_cone(point: Vector3) -> bool:
+	var forward = -global_transform.basis.z
+	var pos = global_transform.origin
+	var dir_to_point = point - pos
+	return rad2deg(dir_to_point.angle_to(forward)) <= vision_cone_arc / 2.0
