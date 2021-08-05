bb792a51ebab24510099e680b4fd8b1b0f5bc169
Mick McCarthy <mick@mccarthy.tech>
2021-08-05 22:38:15 +0100

    Only checking LOS on every second frame

--- VisionCone/VisionManager.gd
+++ VisionCone/VisionManager.gd
@@ -2,6 +2,9 @@ extends Spatial
 
 export var vision_cone_arc: float = 60.0
 
+var los: bool = false
+var check_this_frame: bool = false
+
 func in_vision_cone(point: Vector3) -> bool:
 	var forward = -global_transform.basis.z
 	var pos = global_transform.origin
@@ -9,7 +12,10 @@ func in_vision_cone(point: Vector3) -> bool:
 	return rad2deg(dir_to_point.angle_to(forward)) <= vision_cone_arc / 2.0
 
 func has_los(point: Vector3) -> bool:
+	check_this_frame = !check_this_frame
+	if !check_this_frame:
+		return los
 	var space_state = get_world().direct_space_state
 	var result = space_state.intersect_ray(global_transform.origin, point, [], 1)
-	
-	return !result
+	los = result.size() == 0
+	return los
