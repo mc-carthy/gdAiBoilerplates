06b1e07cc0158175d8fd2bab06f9c70d668dece5
Mick McCarthy <mick@mccarthy.tech>
2021-08-06 14:12:24 +0100

    Rotating on x-axis also

--- FaceTarget/TurnAgent.gd
+++ FaceTarget/TurnAgent.gd
@@ -1,6 +1,7 @@
 extends Spatial
 
-onready var face_target = $FaceTargetY
+onready var face_target_y = $FaceTargetY
+onready var face_target_x = $FaceTargetY/FaceTargetX
 
 var target : Spatial
 func _ready():
@@ -8,7 +9,8 @@ func _ready():
 
 func _process(delta):
 	var target_pos = target.global_transform.origin
-	face_target.face_point(target_pos, delta)
+	face_target_y.face_point(target_pos, delta)
+	face_target_x.face_point(target_pos, delta)
 
 func show_red():
 	$Graphics/Red.show()
