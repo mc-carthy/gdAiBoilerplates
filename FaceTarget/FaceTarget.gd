1f1c9d924e7ef27142fdb8278ed0503883a19185
Mick McCarthy <mick@mccarthy.tech>
2021-08-06 14:06:46 +0100

    Fixing jitter

--- FaceTarget/FaceTarget.gd
+++ FaceTarget/FaceTarget.gd
@@ -7,5 +7,8 @@ func face_point(point: Vector3, delta: float):
 	local_point.y = 0.0
 	var turn_dir = sign(local_point.x)
 	var turn_amount = deg2rad(turn_speed * delta)
+	var angle = Vector3.FORWARD.angle_to(local_point)
 	
+	if angle < turn_amount:
+		turn_amount = angle
 	rotate_object_local(Vector3.UP, -turn_amount * turn_dir)
