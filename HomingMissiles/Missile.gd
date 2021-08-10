e637de5a4729f938f6478c0740f948b118456287
Mick McCarthy <mick@mccarthy.tech>
2021-08-10 18:20:45 +0100

    Initial translation work for homing missile done

--- HomingMissiles/Missile.gd
+++ HomingMissiles/Missile.gd
@@ -5,16 +5,27 @@ var target : Spatial
 var move_speed = 0.0
 var exploded = false
 
+var face_target_x
+var face_target_y
 
 func setup(_target, _move_speed, turn_speed):
 	target = _target
 	move_speed = _move_speed
+	face_target_x = $FaceTargetY/FaceTargetX
+	face_target_y = $FaceTargetY
+	
+	face_target_x.turn_speed = turn_speed
+	face_target_y.turn_speed = turn_speed
 
 func _physics_process(delta):
 	if !is_instance_valid(target):
 		return
 	
-	var move_dir = -global_transform.basis.z
+	var target_pos = target.global_transform.origin
+	face_target_y.face_point(target_pos, delta)
+	face_target_x.face_point(target_pos, delta)
+	
+	var move_dir = -$FaceTargetY/FaceTargetX/DirectionRef.global_transform.basis.z
 	var coll = move_and_collide(move_dir * move_speed * delta)
 	
 	if coll:
