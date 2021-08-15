0cdba18bcbbd47b8fa5603bbb1ba9ec493cc1a10
Mick McCarthy <mick@mccarthy.tech>
2021-08-15 22:33:55 +0100

    Initial pathfinding done

--- Pathfinding/Agent.gd
+++ Pathfinding/Agent.gd
@@ -16,4 +16,9 @@ func _physics_process(delta):
 	global_translate(move_vec * move_speed * delta)
 
 func update_move_vec():
-	pass
+	var path = nav.get_simple_path(global_transform.origin, target.global_transform.origin)
+	if path.size() > 1:
+		var dir: Vector3 = path[1] - global_transform.origin
+		dir.y = 0
+		dir = dir.normalized()
+		move_vec = dir
